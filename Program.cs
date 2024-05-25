e2ac4ad1e7624fee814c037b58496a44b097eeeb
Mansteel555 <75836698+Mansteel555@users.noreply.github.com>
2024-05-25 16:23:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello!");
+
+        Potato x = new Potato();
+
+        Potato y = new Potato();
+
+        y.name = "Nein";
+
+        Console.WriteLine(x.name);
+    }
+
+
+    public class Potato
+    {
+
+        int age = 3;
+
+        float weigth = 6f;
+
+        public string name = "Jojo";
+
+
+    }
+}
